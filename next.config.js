99e48d1e9a850136c1cfdbc48a6fbbaf51e643a2
rschain <keassebojosias@gmail.com>
2022-09-29 11:38:14 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["ipfs.infura.io"]
-  },
-  i18n: {
-    locales: ['fr', 'en-US'],
-    defaultLocale: 'fr',
-  },
-
 }
-
 
 module.exports = nextConfig
