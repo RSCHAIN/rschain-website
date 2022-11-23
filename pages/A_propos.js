b4262b4fdb7029a91b73b04007673bdc284ff7fa
rschain <keassebojosias@gmail.com>
2022-11-23 20:10:29 +0100

    text modification

--- pages/A_propos.js
+++ pages/A_propos.js
@@ -56,9 +56,14 @@ export default function Apropo() {
                         </Text>
                         <Heading>Rschain</Heading>
                         <Text color={'gray.500'} fontSize={'lg'}>
-                        Fondé par le dr. ADJA Elloh Yves Christian, enseignant-chercheur de renommée internationale, RSCHAIN voit le jour
-                        en avril 2022 en Côte d'Ivoire dans la commune d'Abidjan. Sis à Cocody angré 8e tranche;
-                        Rschain est une société à responsabilité limitée unipersonnelle (SARL).
+                        Fondé par le Dr. ADJA Elloh Yves Christian, enseignant-chercheur a l'EPITA, 
+                        RSchain voit le jour en Octobre 2021 en FRANCE. Sis à Massy dans l'Essone,
+                        RSchain entreprise axé sur la recherche et l'implémentation des nouvelles 
+                        technologies dans le domaine de la blockchain.<br> </br>
+
+                        Filiale
+                        La première filiale d'RSCHAIN voit le jour en avril 2022 en Côte d'Ivoire dans la 
+                        commune d'Abidjan. Sis à Cocody angré 8e tranche.
                         </Text>
                        
                     </Stack>
